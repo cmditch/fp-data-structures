6a9d4cdda1613039f33f8a854a5d251c2fc809f4
Coury Ditch <cmditch@gmail.com>
2018-01-20 17:12:39 -0700

    suffixes

--- src/StackData.hs
+++ src/StackData.hs
@@ -6,21 +6,6 @@ module StackData where
 import           Data.Maybe
 
 
--- | Playing with Data
-
-a :: Stack Int
-a = 1 |: 2 |: 3 |: 4 |: 5 |: empty
-
-doubleList :: Funktor f => f Int -> f Int
-doubleList = mapp (* 2)
-
-stack = repeet 42 42
-
-someOps :: Stack Int
-someOps = mapp (*3) $ updateAtIndex (*99) 10 $ mapp (*5) $ replaceAtIndex 818 20 stack
-
--- | Data
-
 data Stack a = Nil | Cons a (Stack a)
     deriving (Eq, Show)
 
@@ -67,10 +52,21 @@ repeet n a = Cons a (repeet (n-1) a)
 lengthh :: Stack a -> Int
 lengthh = lengthh' 0
     where
-        lengthh' 0 Nil         = 0
-        lengthh' n Nil         = n-1
+        lengthh' n Nil         = n
         lengthh' n (Cons x xs) = lengthh' (n+1) xs
 
+
+suffixes :: Stack a -> Stack (Stack a)
+suffixes Nil           = Cons Nil Nil
+suffixes a@(Cons x xs) = Cons a (suffixes xs)
+
+
+toList :: Stack a -> [a]
+toList = toList' []
+    where
+        reverse'                  = foldl (flip (:))
+        toList' accum Nil         = reverse' [] accum
+        toList' accum (Cons x xs) = toList' (x : accum) xs
 
 
 class Funktor f where
@@ -86,3 +82,18 @@ infixr 1 |:
 
 (|:) :: a -> Stack a -> Stack a
 a |: b = Cons a b
+
+
+
+-- | Playing with Data
+
+a :: Stack Int
+a = 1 |: 2 |: 3 |: 4 |: empty
+
+doubleList :: Funktor f => f Int -> f Int
+doubleList = mapp (* 2)
+
+stack = repeet 42 42
+
+someOps :: Stack Int
+someOps = mapp (*3) $ updateAtIndex (*99) 10 $ mapp (*5) $ replaceAtIndex 818 20 stack
